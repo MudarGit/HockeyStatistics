9cddbf1279b37da4254b1556d5d1faf9b755ecc9
MudarGit <kneehighmud@gmail.com>
2015-06-01 12:26:09 -0400

    Initial Commit

---
+++ HockeyStatistics/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HockeyStatistics.MvcApplication" Language="C#" %>
---
+++ HockeyStatistics/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HockeyStatistics.Startup))]
+namespace HockeyStatistics
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
