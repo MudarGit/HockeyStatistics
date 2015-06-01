73e4f4709d3967e14fa9b7368410ad79221984ba
MudarGit <kneehighmud@gmail.com>
2015-06-01 12:41:06 -0400

    + Stats

---
+++ HockeyStatistics/Models/Stats.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HockeyStatistics.Models
+{
+    public class Stats
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public double GamesPlayed { get; set; }
+        public double Goals { get; set; }
+        public double Wins { get; set; }
+        public double Losses { get; set; }
+
+        public double WinPercentage { get; set; }
+
+        [Key]
+        public int Rank { get; set; }
+    }
+}
